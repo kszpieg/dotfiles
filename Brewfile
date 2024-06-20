3a7239182786f83c5d3c063a4be36eb8c08200b0
Krzysztof Szpieg <krzysztof@szpieg.dev>
2024-06-20 19:33:20 +0200

    setup: change order, make it more os flexible

--- Brewfile
+++ Brewfile
@@ -6,3 +6,7 @@ brew 'dog'
 brew 'fastfetch'
 brew 'lsd'
 brew 'tldr'
+brew 'zsh' if OS.mac? # install only on Mac
+brew 'curl' if OS.mac? # install only on Mac
+brew 'bat' if OS.mac? # install only on Mac
+brew 'htop' if OS.mac? # install only on Mac
--- install.sh
+++ install.sh
@@ -6,59 +6,87 @@ function create_ln() {
     FILE=$(basename "$1")
 
     if [ "$2" = "-nf" ]; then
-        echo -ne "Creating symlink of $FILE to $HOME/$FILE  "
+        echo -ne "creating symlink of $FILE to $HOME/$FILE  "
         ln -sfn "$DIR/$1" "$HOME/$FILE"
         echo DONE
     else
-        echo -ne "Creating symlink of $1 to $HOME/$1  "
+        echo -ne "creating symlink of $1 to $HOME/$1  "
         ln -sfn "$DIR/$1" "$HOME/$1"
         echo DONE
     fi
 }
 
 # === Base setup section ===
-echo "INFO: SETTING UP YOUR COMPUTER..."
+echo "=====setting up your computer... please wait...====="
 
-# run latest updates
-sudo apt update -y && sudo apt upgrade -y
+if [ "$OSTYPE" = "linux-gnu" ]; then
+    # run latest updates
+    echo "=====checking for updates... please wait...====="
+    sudo apt update -y && sudo apt upgrade -y
 
-# check for curl and install if it's not found
-echo "Checking for curl..."
-if test ! "$(which curl)"; then
-    sudo apt install curl -y
+    # check for curl and install if it's not found
+    echo "=====checking for curl...====="
+    if test ! "$(which curl)"; then
+        sudo apt install curl -y
+    fi
+    echo DONE
+elif [ "$OSTYPE" = "darwin" ]; then
+    # run latest updates
+    echo "=====checking for updates... please wait...====="
+    sudo softwareupdate -i -a
+fi
+
+# check for homebrew and install if it's not found
+echo "=====checking for homebrew...====="
+if test ! "$(which brew)"; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 echo DONE
 
-# check for zsh and install if it's not found
-echo "Checking for zsh..."
-if test ! "$(which zsh)"; then
-    sudo apt install zsh -y
+# === Homebrew section ===
+echo "=====installing brew packages...====="
+
+# update homebrew recipes
+brew update
+
+# install brew dependencies with bundle
+brew tap homebrew/bundle
+brew bundle --file ./Brewfile
+
+if [ "$OSTYPE" = "linux-gnu" ]; then
+    # check for zsh and install if it's not found
+    echo "=====checking for zsh...====="
+    if test ! "$(which zsh)"; then
+        sudo apt install zsh -y
+    fi
+    echo DONE
+
+    # === App section
+    echo "=====installing linux apps...====="
+
+    # install batcat
+    sudo apt install bat -y
+
+    # install htop
+    sudo apt install htop -y
 fi
-echo DONE
 
 # check for oh-my-zsh and install if it's not found
-echo "Checking for oh-my-zsh..."
+echo "=====checking for oh-my-zsh...====="
 if test ! "$(which omz)"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 echo DONE
 
 # check for starship and install if it's not found
-echo "Checking for starship..."
+echo "=====checking for starship...====="
 if test ! "$(which starship)"; then
     curl -sS https://starship.rs/install.sh | sh
 fi
 echo DONE
 
-# check for homebrew and install if it's not found
-echo "Checking for homebrew..."
-if test ! "$(which brew)"; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-echo DONE
-
 # === Symlinks section ===
-echo "INFO: CREATING SYMLINKS..."
+echo "=====creating symlinks...====="
 
 # remove .zshrc from $HOME (if it exists)
 rm -rf "$HOME/.zshrc"
@@ -70,26 +98,11 @@ create_ln git/.gitconfig -nf
 create_ln .config/fastfetch/config.jsonc
 create_ln .config/starship.toml
 
-# === App section
-echo "INFO: INSTALLING APPS..."
-
-# install batcat
-sudo apt install bat -y
-
-# install htop
-sudo apt install htop -y
-
-# === Homebrew section ===
-echo "INFO: INSTALLING BREW PACKAGES..."
-
-# update homebrew recipes
-brew update
-
-# install brew dependencies with bundle
-brew tap homebrew/bundle
-brew bundle --file ./Brewfile
-
 # === Finish section ===
+
+echo "=====almost done...====="
 
 # load zsh config
 source "$HOME/.zshrc"
+
+echo "=====your machine is settled up, enjoy!====="
