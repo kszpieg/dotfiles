5792b7e2db52d5387e8a233c16c897ad84bc5f2f
Krzysztof Szpieg <krzysztof@szpieg.dev>
2025-02-21 01:54:19 +0100

    feat(aliases): bring back clear alias and replace single quotes with doubles

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -15,38 +15,41 @@ alias uu="sudo apt upgrade"
 alias r="sudo apt remove"
 
 # cat alternative
-alias cat='bat'
-alias pcat='bat --style=plain'
+alias cat="bat"
+alias pcat="bat --style=plain"
 
 # ls alternative
-alias ls='lsd --group-directories-first'
-alias lsl='lsd --group-directories-first -lh'
-alias lsa='lsd --group-directories-first -lhA'
+alias ls="lsd --group-directories-first"
+alias lsl="lsd --group-directories-first -lh"
+alias lsa="lsd --group-directories-first -lhA"
 
 # DNS tool - dog
-alias dns='doggo'
+alias dns="doggo"
 
 # jump back n directories at a time
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+# clear
+alias cl="clear"
 
 # python stuff
-alias python='python3'
+alias python="python3"
 
 # jump to work directory
-alias gdir='cd /mnt/e/gitdir'
+alias gdir="cd /mnt/e/gitdir"
 
 # git aliases
 # update submodule recursively to latest remote commit
-alias gsur='gsu --remote --recursive'
+alias gsur="gsu --remote --recursive"
 
 # jump to dotfiles
-alias dotfiles='cd /mnt/e/gitdir/dotfiles'
+alias dotfiles="cd /mnt/e/gitdir/dotfiles"
 
 # weather
-alias wroclaw='curl wttr.in/wroclaw'
-alias przemysl='curl wttr.in/przemysl'
-alias krakow='curl wttr.in/krakow'
+alias wroclaw="curl wttr.in/wroclaw"
+alias przemysl="curl wttr.in/przemysl"
+alias krakow="curl wttr.in/krakow"
